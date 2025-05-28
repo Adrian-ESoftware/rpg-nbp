b8e93b0b0f7e9c376d9981f2ee023b761abdb3e3
Adrian-ESoftware <rezendeadrian007@gmail.com>
2025-05-28 19:31:27 -0300

    Add primary-icon color variables and SVG fill classes for themed icons
    - Introduced new CSS variables for darker shades of primary colors (e.g., --primary-icon).
    - Added SVG fill classes to support theming for icons.
    - Ensured SVGs are responsive with defined max dimensions.

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -6,21 +6,14 @@ import {
   SidebarHeader,
 } from "@/components/ui/sidebar"
 import SidebarMenuItems from "../layout/SidebarMenuItems"
-import Image from "next/image"
+import ThemedIcon from "../svg/ThemedIcon"
 
 export default function LayoutSidebar() {
     return (
           <Sidebar className="bg-card border-r border-border">
             <SidebarHeader className="bg-card border-b border-border">
               <div className="p-2 flex justify-center">
-                <Image
-                  src="/ico.svg"
-                  alt="RPG Master Logo"
-                  width={180}
-                  height={180}
-                  className="select-none"
-                  priority
-                />
+                <ThemedIcon/>
               </div>
             </SidebarHeader>
             <SidebarContent className="py-8 bg-card">
